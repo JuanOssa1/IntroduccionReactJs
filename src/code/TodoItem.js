551f5ad68bd3f9dfd96795ed54b5abbf744da174
JuanOssa1 <juan.ossa@hotmail.com>
2022-03-18 17:56:39 -0500

    Some hotfixes

--- src/code/TodoItem.js
+++ src/code/TodoItem.js
@@ -4,9 +4,15 @@ import '../Styles/TodoItem.css';
 function TodoItem(props) {
   return (
     <li className="TodoItem">
+            {/**Aqui estamos usando estas comillas invertidas para combinar texto y codigo 
+       * de java script aqui como podemos ver estamos metiendo una condicion en la que si 
+       * el objeto completed es true se va a agregar la clase Icon-check--active esto lo aplicaria para las clases 
+       * Icon y Icon-check por eso tienen el espacio
+       */}
       <span className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}>
         √
       </span>
+
       <p className={`TodoItem-p ${props.completed && 'TodoItem-p--complete'}`}>
         {props.text}
       </p>
