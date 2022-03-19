a299455fd3d74f177cdc017ff5f260d93eee2158
JuanOssa1 <juan.ossa@hotmail.com>
2022-03-19 11:53:04 -0500

    setState agregados

--- src/code/TodoSearch.js
+++ src/code/TodoSearch.js
@@ -2,21 +2,30 @@ import React from 'react';
 import '../Styles/TodoSearch.css';
 
 function TodoSearch() {
-  {/**Esto es como si fuera un observer mira constantemente
+    {/**Esto es como si fuera un observer mira constantemente
 que es lo que ha cambiado y lo va actualizando retorna 
 un objeto y a ese ojet de las tantas propiedades que tiene
-puedo sacarle lo que la persona va escribiendo*/}
+puedo sacarle lo que la persona va escribiendo
+y ese useState es un react Hook*/}
+{/**el primer parametro del arreglo defiene el nombre del estado
+actual y el segundo define el nombre de la funcion para
+actualizar el estado */}
+  const [searchValue, setSearchValue] = React.useState('');
+
   const onSearchValueChange = (event) =>{
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
-  return (
+
+  return [
     <input 
       className="TodoSearch" 
       placeholder="Escribe algo"
       onChange={onSearchValueChange}
-    
-    />
-  );
+      value={searchValue}
+    />,
+    <p>{searchValue}</p>
+  ];
 }
 
 export { TodoSearch };
