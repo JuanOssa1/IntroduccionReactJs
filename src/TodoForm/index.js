d6bcef90d5a90524a6d43d71ee031fe3697d4f27
JuanOssa1 <juan.ossa@hotmail.com>
2022-04-01 11:55:28 -0500

    CSS agregado

--- src/TodoForm/index.js
+++ src/TodoForm/index.js
@@ -1,41 +1,45 @@
-import React from 'react';
-import { TodoContext } from '../TodoContext/TodoContext';
+import React from "react";
+import { TodoContext } from "../TodoContext/TodoContext";
+import './ToDoForm.css'
 /**Textarea es un input grande */
-function TodoForm(){
-    const [newTodoValue, setNewTodoValue] = React.useState('');
-    const {
-        addTodo,
-        setOpenModal,
-    } = React.useContext(TodoContext);
+function TodoForm() {
+  const [newTodoValue, setNewTodoValue] = React.useState("");
+  const { addTodo, setOpenModal } = React.useContext(TodoContext);
 
-    const onCancel = () => {
-        setOpenModal(false);
-    };
-    const onChange = (event) => {
-        setNewTodoValue(event.target.value);
-    };
-    const onSubmit = (event) => {
-        event.preventDefault();
-        addTodo(newTodoValue);
-        setOpenModal(false);
-    };
+  const onCancel = () => {
+    setOpenModal(false);
+  };
+  const onChange = (event) => {
+    setNewTodoValue(event.target.value);
+  };
+  const onSubmit = (event) => {
+    event.preventDefault();
+    addTodo(newTodoValue);
+    setOpenModal(false);
+  };
 
-    return(
-        <form onSubmit={onSubmit}>
-            <label>...</label>
-            <textarea
-                value = {newTodoValue}
-                onChange={onChange}
-                placeholder='Cortar la cebolla para el almuerzo'
-            />
-            <div>
-                <button type='button' onClick={onCancel}> Cancelar </button>
-                {/**No hago el onClick porque es un submit y lo que hace submit es el form */}
-                <button type='submit'> Añadir </button>
-            </div>
-        </form>
-    );
+  return (
+    <form onSubmit={onSubmit}>
+      <label>Escribe tu nuevo To Do</label>
+      <textarea
+        value={newTodoValue}
+        onChange={onChange}
+        placeholder="Escribe una nueva tarea"
+      />
+      <div className="TodoForm-buttonContainer">
+        <button
+          type="button"
+          className="TodoForm-button TodoForm-button-cancel"
+          onClick={onCancel}
+        >
+          Cancelar
+        </button>
+        {/**No hago el onClick porque es un submit y lo que hace submit es el form */}
+        <button className="TodoForm-button TodoForm-button-add" type="submit">
+          Añadir
+        </button>
+      </div>
+    </form>
+  );
 }
-
-
-export {TodoForm};
+export { TodoForm };
